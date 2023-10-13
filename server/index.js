6554497707630279b67cb7ff846df62dce2c0c16
SAI KRISHNA S B <103356323+saigeek@users.noreply.github.com>
2023-10-13 16:48:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const server = app.listen(process.env.PORT,()=> {
 const io= socket(server,{
     cors:{
         origin:process.env.ORIGIN,
-        credential:true,
+        credentials:true,
     }
 });
 
