847566e30ee6cb76d713408dc4d2701421860bf4
SAI KRISHNA S B <103356323+saigeek@users.noreply.github.com>
2023-10-13 17:29:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,16 +30,10 @@ const server = app.listen(process.env.PORT,()=> {
 
 });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://dialougex.netlify.app'); 
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
 const io= socket(server,{
     cors:{
-        origin:'*',
+        origin:'https://dialougex.netlify.app',
         credentials:true,
     }
 });
