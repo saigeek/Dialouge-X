c460c9738cdf22be0be920f5e646ecde322c8049
SAI KRISHNA S B <103356323+saigeek@users.noreply.github.com>
2023-10-15 17:45:25 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://dialouge-x.onrender.com";
+export const host = "https://dialougex.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
